99c36ad46a97b5eb625589587a81fdde8532e3dd
Elena <elena.nisman85@gmail.com>
2022-01-31 18:30:13 +0200

    add methods getAllCharacters and getCharacter in MarvelService

--- src/services/MarvellService.js
+++ src/services/MarvellService.js
@@ -1,6 +1,8 @@
 
-
 class MarvellService {
+_apiBase = 'https://gateway.marvel.com:443/v1/public/';
+_apiKey = 'apikey=8087a107a8ca7d63fae9095e07eebca4'
+
     getResourse = async (url) => {
         let res = await fetch(url);
 
@@ -12,7 +14,11 @@ class MarvellService {
     }
 
     getAllCharacters = () => {
-        return this.getResourse('https://gateway.marvel.com:443/v1/public/characters?apikey=8087a107a8ca7d63fae9095e07eebca4')
+        return this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`)
     }
 }
 export default MarvellService;
