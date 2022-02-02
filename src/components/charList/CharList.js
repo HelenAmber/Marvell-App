0e8da05d0f6f31f390cb86b195661b3a3d2afe57
Elena <elena.nisman85@gmail.com>
2022-02-02 12:53:58 +0200

    Adding lass CharList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,77 @@
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
 
-const CharList = () => {
-    return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+import MarvellService from '../../services/MarvellService';
+import { Component } from 'react/cjs/react.development';
+import Spinner from '../spinner/spinner';
+import ErrorMessage from '../errorMessage/errorMesage'
+
+class CharList extends Component {
+    state = {
+        chars: [],
+        loading: true,
+        error: false
+    }
+    
+    marvellService = new MarvellService();
+
+    componentDidMount() {
+        this.updateChars();
+   }
+
+    onCharsLoaded = (chars) => { 
+        this.setState({
+            chars, 
+            loading: false});      
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true});
+    }
+
+    updateChars = () => {
+        this.marvellService           
+            .getAllCharacters()
+            .then(this.onCharsLoaded)
+            .catch(this.onError);
+    }
+
+    
+    render(){
+        const {chars, loading, error} = this.state;
+        const errorMessage = error ? < ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <View chars={chars}/> : null;
+        
+        return (
+            <div className="char__list">
+                <ul className="char__grid">
+                    {errorMessage}
+                    {content}
+                    {spinner}
+                </ul>
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        ) 
+    }
 }
+
+ const View = ({chars}) => {
+    
+    return chars.map((item) => {
+            const name = item.name;
+            const thumbnail = item.thumbnail;
+        
+            return (
+                <li className="char__item">
+                            <img src={thumbnail} alt="abyss" style={{"objectFit": "unset"}}/>
+                            <div className="char__name">{name}</div>
+                        </li>
+            )
+        })
+ }
 
 export default CharList;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -16,11 +16,6 @@ class RandomChar extends Component {
 
     componentDidMount() {
         this.updateChar();
-        //this.timerId = setInterval(this.updateChar, 3000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerId);
     }
 
     onCharLoaded = (char) => {
